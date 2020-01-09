5e2a2b8f6e039f0f3ff0d6a404078d88b19f4ec9
yuyuOuO <55227155+yuyuOuO@users.noreply.github.com>
2020-01-09 14:15:35 +0800

    20200109

---
+++ Unity_UI_E1080304_LiYuYu/Assets/oooooo/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio; //引用音頻API
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+    //定義欄位(宣告變數)
+    //類型 名稱 結尾
+    //public 公開 、private 私人
+    public AudioMixer mixer;
+    public Text LoadingText;
+    public Slider Loading;
+
+    //定義方法 (宣告函式)
+    //修飾詞 類型 名稱(參數){敘述}
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+    public void Play()
+    {
+        //SceneManager.LoadScene("場景");
+        StartCoroutine(Loading1()); //啟動協同程序(協同程序方法名稱())
+    }
+    //協同程序Coroutine
+    private IEnumerator Loading1()
+    {
+        //print("測試1");
+        //yield return new WaitForSeconds(2); //等待秒數(秒數);
+        //print("測試2");
+        AsyncOperation ao = SceneManager.LoadSceneAsync("Game_demo");  //取得場景資訊
+        ao.allowSceneActivation = false;  //取消載入場景
+        while (ao.isDone == false)
+        {
+            LoadingText.text = ((ao.progress / 0.9f) * 100).ToString(); //更新文字 = 載入.進度
+            Loading.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.01f);
+            if ( ao.progress == 0.9f && Input.anyKey)
+            {
+                ao.allowSceneActivation = true;
+            }
+        }
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("選單");
+        Time.timeScale = 1; 
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity_UI_E1080304_LiYuYu/Assets/oooooo/Scripts/player.cs
@@ -0,0 +1,105 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    [Header("血量血條")]
+    public int hp = 100;
+    public Slider hpSlider;
+    [Header("食物")]
+    public Text textFood;
+    public int FoodCount;
+    public int FoodTotal;
+    [Header("時間區域")]
+    public Text textTime;
+    public float gameTime;
+    [Header("結束畫布")]
+    public GameObject final;
+    public Text textBest;
+    public Text textCurrent;
+    public AudioClip _audioClip;
+    AudioSource _audioSource;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+           hpSlider.value = hp;
+            if (hp <= 0) Dead();
+
+        }
+        if (other.tag == "ITEMS")
+        {
+            FoodCount++;
+            textFood.text = "ITEMS：" + FoodCount + "/" + FoodTotal;
+            _audioSource.PlayOneShot(_audioClip);
+            Destroy(other.gameObject,_audioClip.length);
+
+        }
+        if(other.name == "終點"&& FoodCount == FoodTotal)
+        {
+            print("過關");
+        }
+        if(other.name == "終點"&& FoodCount == FoodTotal)
+        {
+            GameOver();
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+            if (hp <= 0) Dead();
+        }
+    }
+    private void Start()
+    {
+        FoodTotal = GameObject.FindGameObjectsWithTag("ITEMS").Length;
+        _audioSource = GetComponent<AudioSource>();
+        textFood.text = "ITEMS：0/" + FoodTotal;
+
+        if (PlayerPrefs.GetFloat("最佳紀錄") == 0)
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", 99999);
+        }
+    }
+    private void Update()
+    {
+        UpdateTime();
+    }
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("0.00");
+    }
+    private void Dead()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME:" + gameTime.ToString("0.00");
+        textBest.text = "BEST : " + PlayerPrefs.GetFloat("最佳紀錄").ToString("0.00");
+        Cursor.lockState = CursorLockMode.None;
+//        GetComponent<FPSControllerLPFP.FpsControllerLPFP>().enabled = false;
+        this.enabled = false;
+    }
+    private void GameOver()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME:" + gameTime.ToString("0.00");
+        if (gameTime < PlayerPrefs.GetFloat("最佳紀錄"))
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", gameTime);
+        }
+         textBest.text = "BEST : "+PlayerPrefs.GetFloat("最佳紀錄").ToString("0.00") ;
+
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
